831332b98595b5780570692a47c51e6705d105db
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2024-03-24 23:08:55 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,33 +1,81 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 The Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 DEVICE_PATH := device/xiaomi/spes
 
-# Dynamic Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Enable virtual A/B OTA
-ENABLE_VIRTUAL_AB := true
+# Configure Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
+# Enable developer GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Configure emulated_storage.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 32
+# OTA Assert
+TARGET_OTA_ASSERT_DEVICE := spes,spesn
+
+# Boot control, Firmware
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    miui_prebuilt
+
+# FastbootD support
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd
+
+# Update engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    checkpoint_gc
 
 # API
 PRODUCT_SHIPPING_API_LEVEL  := 31
+PRODUCT_TARGET_VNDK_VERSION := 33
 
-# A/B
+# Display Size & Density
+TARGET_SCREEN_HEIGHT  := 2400
+TARGET_SCREEN_DENSITY := 390
+TARGET_SCREEN_WIDTH   := 1080
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION  := false
+
+# Virtual A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    init_boot \
+    odm \
+    product \
+    recovery \
+    system \
+    system_dlkm \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor \
+    vendor_boot \
+    vendor_dlkm
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
@@ -40,54 +88,17 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_vendor=ext4 \
     POSTINSTALL_OPTIONAL_vendor=true
 
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    checkpoint_gc
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+	$(DEVICE_PATH) \
+	vendor/qcom/opensource/commonsys-intf/display
 
-# Boot control
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-impl-qti.recovery \
-    android.hardware.boot@1.2-service
-
-# f2fs utilities
-PRODUCT_PACKAGES += \
-    sg_write_buffer \
-    f2fs_io \
-    check_f2fs
-
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
-    fastbootd
-
-# Init scripts
-PRODUCT_PACKAGES += \
-    init.recovery.qcom.sh \
-    init.recovery.qcom.rc
-
-# Update engine
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-
-PRODUCT_PACKAGES += \
-    qcom_decrypt \
-    qcom_decrypt_fbe
-
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone7/temp"
+TW_BRIGHTNESS_PATH      := "/sys/class/backlight/panel0-backlight/brightness"
 TARGET_RECOVERY_DEVICE_MODULES += libion vendor.display.config@1.0 vendor.display.config@2.0 libdisplayconfig.qti vendor.qti.hardware.vibrator.service vendor.qti.hardware.vibrator.impl libqtivibratoreffect
 
-#Properties
-TW_OVERRIDE_SYSTEM_PROPS := \
-    "ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental"
-
-#OF_DEFAULT_KEYMASTER_VERSION This is to specify the default version for the keymaster services used for decryption
-OF_DEFAULT_KEYMASTER_VERSION := 4.1
-TW_FORCE_KEYMASTER_VER := true
+TW_BATTERY_SYSFS_WAIT_SECONDS := 6
+TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
 
 # Vibrator
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
@@ -96,10 +107,6 @@ RECOVERY_BINARY_SOURCE_FILES += \
     $(TARGET_OUT_VENDOR_EXECUTABLES)/hw/vendor.qti.hardware.vibrator.service
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/vendor.qti.hardware.vibrator.impl.so \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libqtivibratoreffect.so
 
